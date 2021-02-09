8bb8575a49b7b18ad626d1c883e0d0ca4c86f2c9
ve3 <kawewong@gmail.com>
2021-02-09 15:24:51 +0700

    Update permissions in menu items.

--- ModuleData/ModuleAdmin.php
+++ ModuleData/ModuleAdmin.php
@@ -133,7 +133,10 @@ class ModuleAdmin implements \Rdb\Modules\RdbAdmin\Interfaces\ModuleAdmin
                 'id' => 'rdbcmsa-contents-menu',
                 'permission' => [
                     ['RdbCMSAContentCategories', 'list', 'add', 'edit', 'delete'],
+                    ['RdbCMSAContentTags', 'list', 'add', 'edit', 'delete'],
                     ['RdbCMSAPosts', 'list', 'add', 'edit', 'delete'],
+                    ['RdbCMSAPages', 'list', 'add', 'edit', 'delete'],
+                    ['RdbCMSAFiles', 'list', 'add', 'edit', 'delete'],
                 ],
                 'icon' => 'fas fa-file-alt fa-fw',
                 'name' => d__('rdbcmsa', 'Contents'),
