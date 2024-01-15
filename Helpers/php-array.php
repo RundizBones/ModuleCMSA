9cb8dd2d8e7f90f58f25c1988a327a7c56fa39b8
ve3 <kawewong@gmail.com>
2024-01-15 12:28:50 +0700

    Add `array_is_list()` polyfill.

--- Helpers/php-array.php
+++ Helpers/php-array.php
@@ -6,19 +6,46 @@
  */
 
 
+if (!function_exists('array_is_list')) {
+    /**
+     * Checks whether a given array is a list
+     * 
+     * @link https://www.php.net/manual/en/function.array-is-list.php#127044 Original source code.
+     * @link https://www.php.net/manual/en/function.array-is-list.php PHP document.
+     * @link https://3v4l.org/paaK1 Benchmark result.
+     * @param array $arr The array being evaluated.
+     * @return bool Returns `true` if array is a list, `false` otherwise.
+     */
+    function array_is_list(array $array): bool
+    {
+        $i = -1;
+        foreach ($array as $k => $v) {
+            ++$i;
+            if ($k !== $i) {
+                return false;
+            }
+        }
+
+        return true;
+    }// array_is_list
+}
+
+
 if (!function_exists('array_key_last')) {
     /**
      * Gets the last key of an array
      * 
      * @link https://www.php.net/manual/en/function.array-key-last.php#123016 Original source code.
+     * @link https://www.php.net/manual/en/function.array-key-last.php PHP document.
      * @param array $array An array.
-     * @return int|string|null Returns the last key of array if the array is not empty; null otherwise.
+     * @return int|string|null Returns the last key of array if the array is not empty; `null` otherwise.
      */
-    function array_key_last(array $array) {
+    function array_key_last(array $array)
+    {
         if (!is_array($array) || empty($array)) {
             return NULL;
         }
        
         return array_keys($array)[count($array)-1];
-    }
+    }// array_key_last
 }
